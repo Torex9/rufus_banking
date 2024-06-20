3256ec6e031aa70aa255984836702b4adc257d28
Torex9 <92604396+Torex9@users.noreply.github.com>
2024-06-20 19:33:22 +0100

    implemented bank cards and menu navbar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Rufus" };
+  const loggedIn = {
+    firstName: "Rufus",
+    lastName: "Oludare",
+    email: "toludare68@gmail.com",
+  };
 
   return (
     <section className="home">
@@ -21,7 +26,9 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
     </section>
   );
 };
