ff2eab232b0e45bb341c5bedad0593501382a630
Torex9 <92604396+Torex9@users.noreply.github.com>
2024-07-20 23:33:53 +0100

    started implementing  transactions

---
+++ components/TransactionsTable.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const TransactionsTable = () => {
+  return (
+    <Table>
+      <TableHeader className="bg-[#f9fafb]">
+        <TableRow>
+          <TableHead className="px-2">Transaction</TableHead>
+          <TableHead className="px-2">Amount</TableHead>
+          <TableHead className="px-2">Status</TableHead>
+          <TableHead className="px-2">Date</TableHead>
+          <TableHead className="px-2 max-md:hidden">Channel</TableHead>
+          <TableHead className="px-2 max-md:hidden">Category</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell className="font-medium">INV001</TableCell>
+          <TableCell>Paid</TableCell>
+          <TableCell>Credit Card</TableCell>
+          <TableCell className="text-right">$250.00</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
+};
+
+export default TransactionsTable;
